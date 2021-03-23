fc405f5021792b25335f0580f9985dcad0da0ecd
unknown <somesh333>
2021-03-23 18:47:33 +0530

    Ecommerce react

---
+++ src/pages/homepage/homepage.comp.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                      <h1 className="title">Hats</h1>
+                      <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                      <h1 className="title">Jakets</h1>
+                      <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                      <h1 className="title">Snekers</h1>
+                      <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                      <h1 className="title">Mens</h1>
+                      <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                      <h1 className="title">Womens</h1>
+                      <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
